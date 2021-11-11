4e74b7fbda25d6377ee54b6308453bb64b38cfac
Mees van Dongen <mvandongen@ilionx.com>
2021-11-11 10:01:01 +0100

    fix typings of iframe stores

--- packages/oauth-client-core/src/utils/iframe.ts
+++ packages/oauth-client-core/src/utils/iframe.ts
@@ -17,7 +17,7 @@ export function destroyIframe(iFrame: HTMLIFrameElement): void {
  * Promise.
  */
 const iframeStore: {
-  [key: string]: Promise<string>;
+  [key: string]: Promise<string> | undefined;
 } = {};
 
 /**
@@ -27,8 +27,9 @@ const iframeStore: {
  * @returns the loaded url
  */
 export function loadIframeUrl(url: string): Promise<string> {
-  if (iframeStore[url]) {
-    return iframeStore[url];
+  const storedPromise = iframeStore[url];
+  if (storedPromise) {
+    return storedPromise;
   }
 
   const promise = new Promise<string>((resolve, reject) => {
